be0c79de9fc9a861b273e26418f23000ace409c7
Guillaume Groult <guillaume.groult3@orange.fr>
2021-01-18 01:12:00 +0100

    Redéfinition d'opérateurs pour la classe Vect

--- main.cpp
+++ main.cpp
@@ -16,6 +16,40 @@ using namespace std;
 
 //#define M_PI 3.141592653589793238562;
 
+#include <vect.h>
+
+Vect operator+(const Vect& a, const Vect& b){
+    return Vect(a[0] + b[0],a[1] + b[1],a[2] + b[2]);
+}
+
+Vect operator-(const Vect& a, const Vect& b){
+    return Vect(a[0] - b[0],a[1] - b[1],a[2] - b[2]);
+}
+
+Vect operator-(const Vect& a){
+    return Vect(-a[0],-a[1],-a[2]);
+}
+
+Vect operator*(const Vect& a, double b){
+    return Vect(a[0]*b,a[1]*b,a[2]*b);
+}
+
+Vect operator*(double a,const Vect& b){
+    return Vect(a*b[0],a*b[1],a*b[2]);
+}
+
+Vect operator/(const Vect& a, double b){
+    return Vect(a[0]/b,a[1]/b,a[2]/b);
+}
+
+double dot(const Vect& a,const Vect& b){
+    return a[0]*b[0] + a[1]*b[1] + a[2]*b[2];
+}
+
+double sqr(double x){
+    return x*x;
+}
+
 int main()
 {
     time_t beginTime,endTime;
