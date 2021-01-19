8afc7a7654d0f6b948077ea68e4c93249a659cd7
Guillaume Groult <guillaume.groult3@orange.fr>
2021-01-19 21:52:12 +0100

    Définition de la classe Vect

--- vect.cpp
+++ vect.cpp
@@ -1,6 +1,25 @@
-#include "vector.h"
+#include "vect.h"
+#include <math.h>
 
-Vector::Vector()
-{
+Vect::Vect(double x,double y,double z){
+    coords[0] = x;
+    coords[1] = y;
+    coords[2] = z;
+}
 
+double Vect::operator[](int i) const{
+    return coords[i];
+}
+
+double& Vect::operator[](int i){
+    return coords[i];
+}
+
+double Vect::sqrNorm(){
+    return coords[0] * coords[0] + coords[1] * coords[1] + coords[2] * coords[2];
+}
+
+Vect Vect::get_normalized(){
+    double n = sqrt(sqrNorm());
+    return Vect(coords[0] / n,coords[1] / n,coords[2] / n);
 }
--- vect.h
+++ vect.h
@@ -1,11 +1,19 @@
-#ifndef VECTOR_H
-#define VECTOR_H
+#ifndef VECT_H
+#define VECT_H
 
 
-class Vector
+class Vect
 {
 public:
-    Vector();
+    explicit Vect(double x=0,double y=0,double z=0);
+    double operator[](int i) const;
+    double &operator[](int i);
+    double sqrNorm();
+    Vect get_normalized();
+
+private:
+    double coords[3];
+
 };
 
-#endif // VECTOR_H
+#endif // VECT_H
